a3522a9e2f6331b85c6bf45ff68d1ddfd6dbff0a
fahimhossainmunna <hossainfahim306@gmail.com>
2025-12-19 02:52:00 +0600

    solve

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins,Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
